2c387cdeaac0dbe4c6beb8a6262c5702df5b4d85
armmar87 <30770514+armmar87@users.noreply.github.com>
2021-04-26 10:10:13 +0400

    Update Home.js
    Change text

--- resources/js/components/Home.js
+++ resources/js/components/Home.js
@@ -4,8 +4,8 @@ import {Link} from "react-router-dom";
 export default function Home() {
     return (
         <div className='home'>
-            <h3>Start a game</h3>
-            <Link className="nav-link" to="/start-play">Play</Link>
+            <h3>Millionaire</h3>
+            <Link className="nav-link" to="/start-play">Start a game</Link>
         </div>
     )
 }
